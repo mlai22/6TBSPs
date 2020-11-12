27cdf5d474f5ec9a7bb1229293d423c1e5991f29
Zitong <zitong@ZT-MBP.local>
2020-11-12 18:43:28 -0500

    traceback done

--- local_alignment.py
+++ local_alignment.py
@@ -2,20 +2,19 @@ import sys
 import os
 import numpy as np
 
-
-class LocalAlignment:
-    def __init__(self, seq_x, seq_y):
-        '''init class paras and score_matrix'''
-        self.V = np.zeros((len(seq_x)+1, len(seq_y)+1), dtype=int)
-        self.x = seq_x
-        self.y = seq_y
-        self.s = self.test_score_matrix
-
-    def test_score_matrix(self, xc, yc):
+def test_score_matrix(xc, yc):
         ''' Cost function: 2 to match, -6 to gap, -4 to mismatch '''
         if xc == yc: return 2 # match
         if xc == '-' or yc == '-': return -6 # gap
         return -4
+
+class LocalAlignment:
+    def __init__(self, seq_x, seq_y, score_matrix):
+        '''init class paras and score_matrix'''
+        self.V = np.zeros((len(seq_x)+1, len(seq_y)+1), dtype=int)
+        self.x = seq_x
+        self.y = seq_y
+        self.s = score_matrix
 
     def fill_matrix(self):
         ''' Calculate local alignment values of sequences x and y using
@@ -30,14 +29,44 @@ class LocalAlignment:
         self.V_max = int(self.V[argmax])
     
     def traceback(self):
-        pass
+        """ Trace back from given cell in local-alignment matrix V """
+        # get i, j for maximal cell
+        i, j = np.unravel_index(np.argmax(self.V), self.V.shape)
+        # traceback
+        xscript, alx, aly, alm = [], [], [], []
+        while (i > 0 or j > 0) and self.V[i, j] != 0:
+            diag, vert, horz = 0, 0, 0
+            if i > 0 and j > 0:
+                diag = self.V[i-1, j-1] + self.s(self.x[i-1], self.y[j-1])
+            if i > 0:
+                vert = self.V[i-1, j] +   self.s(self.x[i-1], '-')
+            if j > 0:
+                horz = self.V[i, j-1] +   self.s('-', self.y[j-1])
+            if diag >= vert and diag >= horz:
+                match = self.x[i-1] == self.y[j-1]
+                xscript.append('M' if match else 'R')
+                alm.append('|' if match else ' ')
+                alx.append(self.x[i-1]); aly.append(self.y[j-1])
+                i -= 1; j -= 1
+            elif vert >= horz:
+                xscript.append('D')
+                alx.append(self.x[i-1]); aly.append('-'); alm.append(' ')
+                i -= 1
+            else:
+                xscript.append('I')
+                aly.append(self.y[j-1]); alx.append('-'); alm.append(' ')
+                j -= 1
+        self.xscript = (''.join(xscript))[::-1]
+        self.alignment = '\n'.join(map(lambda l: ''.join(l), [alx[::-1], alm[::-1], aly[::-1]]))
 
     def display(self):
         print(self.V) 
         print(self.V_max)
+        print(self.alignment)
 
 
 if __name__ == "__main__":
-    la = LocalAlignment('GGTATGCTGGCGCTA', 'TATATGCGGCGTTT')
+    la = LocalAlignment('GGTATGCTGGCGCTA', 'TATATGCGGCGTTT', test_score_matrix)
     la.fill_matrix()
+    la.traceback()
     la.display()
